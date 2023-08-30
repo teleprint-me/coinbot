14a0b1cd52741c04a49fc56a09dd0eb58d660a31
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-30 10:02:42 -0400

    refactor: Fix ellipsis in doc string quote
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- coinbot/__init__.py
+++ coinbot/__init__.py
@@ -1,4 +1,4 @@
 """
-"Compound interest is the eighth wonder of the world. He who understands it, earns it ... he who doesn't ... pays it."
+"Compound interest is the eighth wonder of the world. He who understands it, earns it... he who doesn't... pays it."
     - Albert Einstein
 """
