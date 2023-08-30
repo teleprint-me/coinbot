be797924f636444cc98ae613713ac844f57dbd08
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-30 10:15:34 -0400

    refactor: Add magic values for version, ownership, and email
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- coinbot/__init__.py
+++ coinbot/__init__.py
@@ -1,4 +1,11 @@
 """
 "Compound interest is the eighth wonder of the world. He who understands it, earns it... he who doesn't... pays it."
     - Albert Einstein
+
+coinbot/__init__.py
 """
+
+__version__ = "0.0.2"
+__name__ = "coinbot"
+__author__ = "Austin Berrio"
+__email__ = "aberrio@teleprint.me"
