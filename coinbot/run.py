f1105355a97c2d0f83e8613b0ceb1a36e3fc06d4
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-30 18:52:55 -0400

    refactor: Improved ValueAveraging Prototype
    - Parameterized the constructor for greater flexibility
    - Utilized the 'decimal' module for enhanced precision handling
    - Refined calculation methods to align with precision requirements
    - Updated example usage to showcase the changes
    
    This refactoring enhances the accuracy and flexibility of the ValueAveraging prototype, ensuring more reliable results in different scenarios.

--- coinbot/run.py
+++ coinbot/run.py
@@ -1,29 +1,49 @@
-class ValueAveraging:
-    def __init__(self):
-        self.interval = 1
-        self.prev_total_order_size = 0
-        self.prev_total_trade_amount = 0
-        self.principal_amount = 10
-        self.interest_rate = 0.10
-        self.frequency = 12
-        self.growth_rate = 1 + (self.interest_rate / self.frequency)
-        self.records = []
+from decimal import ROUND_HALF_EVEN, Decimal
+from typing import Dict, List, Optional
 
-    @staticmethod
-    def calculate_trade_amount(
-        principal, annual_rate, compounding_frequency, time_period
+
+class ValueAveraging:
+    def __init__(
+        self,
+        principal_amount: float,
+        interest_rate: float,
+        frequency: int,
+        base_precision: Optional[int] = 8,
+        quote_precision: Optional[int] = 2,
     ):
-        # Calculate the target amount using the compound interest formula
-        target_amount = principal * (1 + annual_rate / compounding_frequency) ** (
-            compounding_frequency * time_period
+        self.interval = 1
+        self.prev_total_order_size = self.round_decimal("0", base_precision)
+        self.prev_total_trade_amount = self.round_decimal("0", base_precision)
+        self.principal_amount = self.round_decimal(principal_amount, quote_precision)
+        self.interest_rate = self.round_decimal(interest_rate, quote_precision)
+        self.frequency = frequency
+        self.records: List[Dict[str, str | float | int]] = []
+
+    def round_decimal(self, value, decimal_places) -> Decimal:
+        return Decimal(value).quantize(
+            Decimal(f"1e-{decimal_places}"), rounding=ROUND_HALF_EVEN
         )
 
-        # Calculate the trade amount as the difference between the target amount and the current principal
-        trade_amount = target_amount - principal
+    def calculate_trade_amount(self, target_amount, current_value):
+        # Calculate the trade amount as the difference between the target amount and the current value
+        current_trade_amount = target_amount - current_value
+        return current_trade_amount
 
-        return target_amount, trade_amount
+    def get_target_amount(self, interval):
+        rate_per_compounding_period = self.interest_rate / self.frequency
 
-    def initialize_first_record(self, market_price, datetime):
+        # Calculate the Current Target amount using the adapted Value Averaging formula
+        current_target_amount = (
+            self.principal_amount
+            * interval
+            * (1 + rate_per_compounding_period) ** interval
+        )
+        return current_target_amount
+
+    def initialize_first_record(
+        self, market_price, datetime, precision: Optional[int] = 2
+    ):
+        market_price = self.round_decimal(market_price, precision)
         current_target = self.principal_amount  # Initial principal amount
         order_size = self.principal_amount / market_price
         total_order_size = order_size + self.prev_total_order_size
@@ -32,10 +52,14 @@ class ValueAveraging:
         total_trade_amount = trade_amount + self.prev_total_trade_amount
 
         # Rounding for precision
-        order_size = round(order_size, 8)  # Rounded to 8 decimal places for satoshis
-        total_order_size = round(total_order_size, 8)
-        trade_amount = round(trade_amount, 2)  # Rounded to 2 decimal places for cents
-        total_trade_amount = round(total_trade_amount, 2)
+        order_size = self.round_decimal(
+            order_size, 8
+        )  # Rounded to 8 decimal places for satoshis
+        total_order_size = self.round_decimal(total_order_size, 8)
+        trade_amount = self.round_decimal(
+            trade_amount, 2
+        )  # Rounded to 2 decimal places for cents
+        total_trade_amount = self.round_decimal(total_trade_amount, 2)
 
         # Create a dictionary to store these values
         record = {
@@ -51,8 +75,8 @@ class ValueAveraging:
             "Interval": self.interval,
         }
 
-        # Append this record to our list of records
-        self.records.append(record)
+        # Instantiate the initial record as a list of records
+        self.records = [record]
 
         # Update the class variables for next calculations
         self.prev_total_order_size = total_order_size
@@ -61,6 +85,6 @@ class ValueAveraging:
 
 
 # Example usage:
-va = ValueAveraging()
-va.initialize_first_record(9334.98, "2020-01-01")
+va = ValueAveraging(principal_amount=10, interest_rate=0.05, frequency=12)
+va.initialize_first_record(market_price=9334.98, datetime="2020-01-01")
 print(va.records)
