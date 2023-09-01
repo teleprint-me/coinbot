73d73d9374b92cfc1f4c4be2041177cf35299495
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-01 13:05:18 -0400

    refactor: Update Alpaca API functions and improve error handling
    - Updated function signatures for improved parameter names and typing
    - Changed 'data' parameter to 'params' in 'get' functions
    - Added type hints for function return values and parameters
    - Enhanced error handling and logging for better error messages
    - Improved docstrings for clarity and adherence to PEP-257 guidelines
    - Created 'page_crypto_candlesticks' function for paginated data retrieval
    - Added pagination support and error handling in 'page_crypto_candlesticks'
    
    This commit refactors the Alpaca API functions to improve readability, error handling,
    and consistency. The function signatures have been updated to use clearer parameter names
    and appropriate type hints. Error handling has been enhanced with more informative logging
    and error messages. Additionally, a new function 'page_crypto_candlesticks' has been
    added to handle paginated data retrieval from the Alpaca API.

--- coinbot/api/alpaca.py
+++ coinbot/api/alpaca.py
@@ -2,12 +2,13 @@
 coinbot/api/alpaca.py
 """
 import time
+from collections import defaultdict
 from os import getenv
 from typing import Any, Dict, List, Optional, Union
 
 import requests
 from dotenv import load_dotenv
-from requests import RequestException
+from requests import RequestException, Response
 from requests.auth import AuthBase
 from requests.models import PreparedRequest
 
@@ -80,12 +81,12 @@ class Auth(AuthBase):
 __auth__ = Auth()
 
 
-def get(url: str, data: Optional[Dict] = None) -> Dict[str, Any]:
+def get(url: str, params: Optional[Dict] = None) -> Response:
     """Perform a GET request to the specified API path.
 
     Args:
         path: The API endpoint to be requested.
-        data: (optional) Query parameters to be passed with the request.
+        params: (optional) Query parameters to be passed with the request.
 
     Returns:
         The response of the GET request.
@@ -96,7 +97,7 @@ def get(url: str, data: Optional[Dict] = None) -> Dict[str, Any]:
     try:
         response = requests.get(
             url=url,
-            params=data,
+            params=params,
             auth=__auth__,
             timeout=__timeout__,
         )
@@ -116,24 +117,69 @@ def get(url: str, data: Optional[Dict] = None) -> Dict[str, Any]:
         raise RequestException(error)
 
 
-def get_crypto_candlesticks(loc: str, data: dict[str, Any]) -> dict[str, List]:
+def get_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
     """
     Fetches historical crypto candlestick data from the Alpaca API.
 
     Args:
         loc (str): Crypto location, e.g., "us".
-        data (dict[str, Any]): Parameters for the API request.
+        params (dict[str, Any]): Parameters for the API request.
 
     Returns:
-        dict[str, List]: Historical candlestick data for specified symbols.
+        Dict[str, List[Dict]]: Historical candlestick data for specified symbols.
     """
     url = f"https://data.alpaca.markets/v1beta3/crypto/{loc}/bars"
-    response = get(url, data=data)
-    data = response.json()
+    # NOTE: `get` is a magic function that handles `requests` under the hood.
+    response: Response = get(url, params=params)
+    json_data: Dict[str, Any] = response.json()
 
     if response.status_code != 200:
-        error = data.get("message", "Error retrieving candlesticks from Alpaca API")
-        logging.error(f"RequestError: {response.status_code}: {error}")
+        error = json_data.get("message", "Error getting candlesticks from Alpaca API")
+        logging.error(f"GetError: {response.status_code}: {error}")
         return {}
 
-    return data.get("bars", {})
+    return json_data.get("bars", {})
+
+
+def page_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
+    """
+    Fetches paginated historical crypto candlestick data from the Alpaca API.
+
+    Retrieves candlestick data for specified crypto assets, handling pagination if applicable.
+
+    Args:
+        loc (str): Crypto location, e.g., "us".
+        params (Dict[str, Any]): Parameters for the API request.
+
+    Returns:
+        Dict[str, List[Dict]]: Historical candlestick data for specified symbols, paginated if needed.
+    """
+    url = f"https://data.alpaca.markets/v1beta3/crypto/{loc}/bars"
+    all_bars = defaultdict(list)
+
+    while True:
+        # NOTE: `get` is a magic function that handles `requests` under the hood.
+        response: Response = get(url, params=params)
+        json_data: Dict[str, Any] = response.json()
+
+        if response.status_code != 200:
+            error = json_data.get(
+                "message", "Error paging candlesticks from Alpaca API"
+            )
+            logging.error(f"PageError: {response.status_code}: {error}")
+            break
+
+        bars = json_data.get("bars", {})
+        if not bars:
+            break
+
+        for asset_pair, asset_bars in bars.items():
+            all_bars[asset_pair].extend(asset_bars)
+
+        next_page_token = json_data.get("next_page_token", None)
+        if next_page_token is None:
+            break
+
+        params["page_token"] = next_page_token
+
+    return dict(all_bars)  # Convert defaultdict back to a regular dictionary.
