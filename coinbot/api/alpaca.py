1b3dfe7d9cbd4ae787868f0b5add90f2de10dfb5
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-08-31 22:30:39 -0400

    feat: Add prototyped Alpaca Auth class and get function

--- coinbot/api/alpaca.py
+++ coinbot/api/alpaca.py
@@ -1,8 +1,6 @@
 """
 coinbot/api/alpaca.py
 """
-import hashlib
-import hmac
 import time
 from datetime import datetime as dt
 from os import getenv
@@ -19,7 +17,21 @@ from requests.models import PreparedRequest
 
 from coinbot import __agent__, __source__, __version__
 
+load_dotenv()
+
+# Rate limit of API requests in seconds.
+# Basic is free and allows 200 API calls/min
+# Plus is $99/mo and allows 10,000 API calls/min
+# We default to Basic which allows 200 calls/min
+__limit__: float = 1 / (200 / 60)
+
+# Timeout value for HTTP requests.
+__timeout__: int = 30
+
+# Paper trading URL differs from live trading
 __paper__ = "https://paper-api.alpaca.markets"
+
+# Use the live trading URL
 __alpaca__ = "https://api.alpaca.markets"
 
 
@@ -30,4 +42,79 @@ class Auth(AuthBase):
         api: Instance of the API class, if not provided, a default instance is created.
     """
 
-    pass
+    def __init__(self):
+        """Create an instance of the Auth class.
+
+        Args:
+            api: Instance of the API class, if not provided, a default instance is created.
+        """
+
+        self.key: Optional[str] = (
+            getenv("ALPACA_API_KEY") or getenv("PAPER_ALPACA_API_KEY") or ""
+        )
+        self.secret: Optional[str] = (
+            getenv("ALPACA_API_SECRET") or getenv("PAPER_ALPACA_API_SECRET") or ""
+        )
+
+    def __call__(self, request: PreparedRequest) -> PreparedRequest:
+        """Return the prepared request with updated headers.
+
+        Args:
+            request: A prepared HTTP request.
+
+        Returns:
+            The same request with updated headers.
+        """
+
+        # Sign and authenticate payload.
+        header: Dict = {
+            "User-Agent": f"{__agent__}/{__version__} {__source__}",
+            "APCA-API-KEY-ID": self.key,
+            "APCA-API-SECRET-KEY": self.secret,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+        # Inject payload
+        request.headers.update(header)
+
+        return request
+
+
+__auth__ = Auth()
+
+
+def get(url: str, data: Optional[Dict] = None) -> Dict[str, Any]:
+    """Perform a GET request to the specified API path.
+
+    Args:
+        path: The API endpoint to be requested.
+        data: (optional) Query parameters to be passed with the request.
+
+    Returns:
+        The response of the GET request.
+    """
+
+    time.sleep(__limit__)
+
+    try:
+        response = requests.get(
+            url=url,
+            params=data,
+            auth=__auth__,
+            timeout=__timeout__,
+        )
+
+        if response.status_code != 200:
+            error_message = response.json()["message"]
+
+        if response.status_code == 400:
+            raise RequestException(f"400 Bad Request: {error_message}")
+        elif response.status_code == 401:
+            raise RequestException(f"401 Unauthorized: {error_message}.")
+        elif response.status_code == 403:
+            raise RequestException(f"403 Forbidden: {error_message}.")
+        else:
+            return response
+    except RequestException as error:
+        raise RequestException(error)
