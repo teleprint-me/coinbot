4646a0730f76509bfb9549823dd6c21d5ac00a7b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-02 18:31:46 -0400

    feat: Add Activation layer implementation in activation.py
    - Implement the `Activation` layer class
    - Include forward and backward methods for activation functions
    
    This commit introduces the `Activation` layer implementation in the `activation.py` module. The `Activation` layer is responsible for applying activation functions to the input data during forward propagation and their derivatives during backward propagation. It provides a flexible way to introduce non-linearity into neural network architectures.

---
+++ coinbot/model/activation.py
@@ -0,0 +1,21 @@
+"""
+coinbot/model/activation.py
+"""
+
+import numpy as np
+
+from coinbot.model.base import Layer
+
+
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        self.output = self.activation(self.input)
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        return output_gradient * self.activation_prime(self.input)
