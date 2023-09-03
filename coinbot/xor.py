200fe08840a2b3f57b6e9ffb6fd06e7c193cb6f6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-02 20:56:24 -0400

    Added XOR Neural Network Implementation
    - Added input and output arrays for XOR problem.
    - Defined the network architecture using Dense and Tanh layers.
    - Set the training parameters including the number of epochs and learning rate.
    - Implemented the training loop with forward and backward passes.
    - Printed the loss every 1000 epochs during training.
    - Tested the trained network on the XOR inputs and printed the predictions.
    
    The current implementation is not producing the expected results, and the outputs seem to be converging towards 0.5. Further investigation and adjustments are needed to improve the learning behavior.

--- coinbot/xor.py
+++ coinbot/xor.py
@@ -9,13 +9,42 @@ from coinbot.model.dense import Dense
 from coinbot.model.error import mse, mse_prime
 from coinbot.model.tanh import Tanh
 
-X = []
-Y = []
+# Input for XOR
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 
-network = [Dense(2, 3), Tanh(), Dense(3, 1), Tanh]
+# Output for XOR
+Y = np.array([[0], [1], [1], [0]])
 
-epochs = 10_000
+# Network architecture
+network = [Dense(2, 3), Tanh(), Dense(3, 1), Tanh()]
+
+# Training parameters
+epochs = 10000
 learning_rate = 0.1
 
+# Training loop
 for epoch in range(epochs):
-    ...
+    # Forward pass
+    output = X
+    for layer in network:
+        output = layer.forward(output)
+
+    # Calculate loss
+    loss = mse(Y, output)
+
+    # Backward pass
+    gradient = mse_prime(Y, output)
+    for layer in reversed(network):
+        gradient = layer.backward(gradient, learning_rate)
+
+    # Print loss every 1000 epochs
+    if epoch % 1000 == 0:
+        print(f"Epoch {epoch}, Loss: {loss}")
+
+# Test the network
+output = X
+for layer in network:
+    output = layer.forward(output)
+
+print("Predictions after training:")
+print(output)
