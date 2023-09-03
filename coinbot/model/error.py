8c11f3a2d803013776a2de446576e1d675348abf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-02 20:32:35 -0400

    feat: Implement Mean Squared Error (MSE) and its derivative
    - Implemented the Mean Squared Error (MSE) function in the `coinbot/model/error.py` module.
    - The `mse(y_true, y_pred)` function calculates the mean squared error between the true and predicted values by computing the mean of the squared differences.
    - Implemented the derivative of the MSE, `mse_prime(y_true, y_pred)`, which computes the gradient of the MSE with respect to the predicted values.
    - The `mse_prime(y_true, y_pred)` function uses the formula 2 * (y_pred - y_true) / y_true.size to compute the derivative.

--- coinbot/model/error.py
+++ coinbot/model/error.py
@@ -5,8 +5,8 @@ import numpy as np
 
 
 def mse(y_true, y_pred):
-    ...
+    return np.mean(np.square(y_true - y_pred))
 
 
 def mse_prime(y_true, y_pred):
-    ...
+    return 2 * (y_pred - y_true) / y_true.size
