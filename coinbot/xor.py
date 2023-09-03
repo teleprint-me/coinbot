b5f9e969374c7f1c758c5a875a6f40e28cb95533
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-03 13:26:36 -0400

    refactor: Implement Early Stopping and Tolerance for XOR Training
    - Adjusted learning rate to 0.15 for XOR training in coinbot/xor.py.
    - Modified L2 regularization parameter to 1e-5 for improved regularization.
    - Introduced a tolerance value of 1e-5 to trigger early stopping when the loss change is below the tolerance threshold.
    - Added a variable `prev_loss` to keep track of the previous loss for early stopping comparison.
    - If the change in loss between consecutive epochs is below the tolerance, the training loop stops early and displays an early stopping message.

--- coinbot/xor.py
+++ coinbot/xor.py
@@ -18,8 +18,10 @@ network = [Dense(2, 3), Tanh(), Dense(3, 1), Tanh()]
 
 # Training parameters
 epochs = 10000
-learning_rate = 0.1
-lambda_ = 0.01  # L2 Regularization parameter
+learning_rate = 0.15
+lambda_ = 1e-5  # L2 regularization
+tolerance = 1e-5  # Tolerance for early stopping
+prev_loss = None  # To store the previous loss
 
 # Training loop
 for epoch in range(epochs):
@@ -29,18 +31,19 @@ for epoch in range(epochs):
         output = layer.forward(output)
 
     # Extract weights for regularization from the Dense layers
-    weights = []
-    for layer in network:
-        if isinstance(layer, Dense):
-            weights.append(layer.weights)
-
-    # Concatenate all the weights into one flat array
+    # and concatenate all the weights into one flat array
+    weights = [layer.weights for layer in network if isinstance(layer, Dense)]
     all_weights = np.concatenate([w.flatten() for w in weights])
-
     # Calculate loss with regularization
     loss = regularized_mse(Y, output, all_weights, lambda_)
 
-    # Backward pass (backward pass needs to include regularization)
+    # Check for early stopping
+    if prev_loss is not None:
+        if abs(prev_loss - loss) < tolerance:
+            print(f"Early stopping on epoch {epoch}, Loss: {loss}")
+            break
+
+    # Backward pass
     gradient = mse_prime(Y, output)
     for layer in reversed(network):
         gradient = layer.backward(gradient, learning_rate, lambda_)
@@ -48,6 +51,8 @@ for epoch in range(epochs):
     # Print loss every 1000 epochs
     if epoch % 1000 == 0:
         print(f"Epoch {epoch}, Loss: {loss}")
+
+    prev_loss = loss  # Update the previous loss
 
 # Test the network
 output = X
