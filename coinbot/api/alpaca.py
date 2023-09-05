0ceefa96607ec196cbeb95240c636ebdb4fb49d4
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-05 12:41:22 -0400

    refactor: Update Alpaca API functions and endpoints
    - Refactored Alpaca API functions to use consistent parameter names and added live flag where applicable.
    - Updated endpoint construction using the get_alpaca_endpoint function for better readability and consistency.
    - Added support for live trading data in functions that interact with the Alpaca API.
    - Modified get_amount_from_orders function to use get_alpaca_endpoint and added live flag for fetching closed orders.
    
    This commit improves the Alpaca API integration in the codebase, making it more modular and flexible.

--- coinbot/api/alpaca.py
+++ coinbot/api/alpaca.py
@@ -26,21 +26,67 @@ __limit__: float = 1 / (200 / 60)
 # Timeout value for HTTP requests.
 __timeout__: int = 30
 
-#
-# NOTE: Alpaca has multiple sub-domains depending on the given context.
-#
+# Define the Alpaca API sub-domains based on context
+ALPACA_SUBDOMAINS = {
+    "broker": {
+        "paper": "https://broker-api.sandbox.alpaca.markets",
+        "live": "https://broker-api.alpaca.markets",
+    },
+    "trade": {
+        "paper": "https://paper-api.alpaca.markets",
+        "live": "https://api.alpaca.markets",
+    },
+    "data": {
+        "paper": "https://data.sandbox.alpaca.markets",
+        "live": "https://data.alpaca.markets",
+    },
+}
 
-# BROKER API
-__paper_broker__ = "https://broker-api.sandbox.alpaca.markets"
-__live_broker__ = "https://broker-api.alpaca.markets"
 
-# TRADING API
-__paper_trade__ = "https://paper-api.alpaca.markets"
-__live_trade__ = "https://api.alpaca.markets"
+def normalize_endpoint(endpoint: str) -> str:
+    """Normalize the endpoint by adding a leading slash if missing.
 
-# Market Data API
-__paper_data__ = "https://data.sandbox.alpaca.markets"
-__live_data__ = "https://data.alpaca.markets"
+    Args:
+        endpoint (str): The endpoint to normalize.
+
+    Returns:
+        str: The normalized endpoint.
+    """
+    return endpoint if endpoint.startswith("/") else "/" + endpoint
+
+
+def get_alpaca_url(subdomain: str, context: str, endpoint: str) -> str:
+    """Construct the Alpaca API URL based on sub-domain, context, and endpoint.
+
+    Args:
+        subdomain (str): The API sub-domain ("broker", "trade", or "data").
+        context (str): The context ("paper" for paper trading, "live" for live trading).
+        endpoint (str): The specific endpoint to access.
+
+    Returns:
+        str: The full API URL based on the provided parameters.
+
+    Example:
+        trade_api_url = get_alpaca_url("trade", "live", "/v2/orders")
+        print(trade_api_url)
+    """
+    normalized_endpoint = normalize_endpoint(endpoint)
+    return f"{ALPACA_SUBDOMAINS[subdomain][context]}{normalized_endpoint}"
+
+
+def get_alpaca_endpoint(subdomain: str, endpoint: str, live: bool = False) -> str:
+    """Constructs the Alpaca API endpoint based on sub-domain and live flag.
+
+    Args:
+        subdomain (str): The API sub-domain ("broker", "trade", or "data").
+        endpoint (str): The specific endpoint to access.
+        live (bool): Whether to use live trading data. Default is False.
+
+    Returns:
+        str: The constructed API endpoint.
+    """
+    context = "live" if live else "paper"
+    return get_alpaca_url(subdomain, context, endpoint)
 
 
 class Auth(AuthBase):
@@ -202,7 +248,7 @@ def get_json_data(response: Response) -> Dict[str, Any]:
         raise RequestException(f"Error ({response.status_code}): {response.text}")
 
 
-def get_clock() -> Dict[str, Union[str, bool]]:
+def get_clock(live: bool = True) -> Dict[str, Union[str, bool]]:
     """Returns the market clock.
 
     The clock API serves the current market timestamp, whether or not the market is currently open, as well as the times of the next market open and close.
@@ -212,8 +258,8 @@ def get_clock() -> Dict[str, Union[str, bool]]:
     Returns:
         Dict: A dictionary representing whether the market is open or closed.
     """
-    response = get(f"{__live_trade__}/v2/clock")
-
+    endpoint = get_alpaca_endpoint("trade", "/v2/clock", live)
+    response = get(endpoint)
     return get_json_data(response)
 
 
@@ -225,26 +271,19 @@ def get_asset(symbol: str, live: bool = False) -> Dict[str, Any]:
     Returns:
         Dict: A dictionary containing information about the given asset.
     """
-    endpoint = None
-
-    if live:
-        endpoint = f"{__live_trade__}/v2/assets/{symbol}"
-    else:
-        endpoint = f"{__paper_trade__}/v2/assets/{symbol}"
-
+    endpoint = get_alpaca_endpoint("trade", f"/v2/assets/{symbol}", live=live)
     response = get(endpoint)
-
     return get_json_data(response)
 
 
-def get_current_asset_value(symbol: str, live: bool = True) -> Dict[str, float]:
+def get_current_asset_value(symbol: str, live: bool = False) -> Dict[str, float]:
     """Get the current value and quantity of a specified asset.
 
     Retrieves the current price, quantity, and market value of a specified asset from the positions API.
 
     Args:
         symbol (str): The symbol of the asset (e.g., "AAPL", "BTC/USD").
-        live (bool, optional): Whether to use live trading data. Default is True.
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         Dict[str, float]: A dictionary containing current price, quantity, and market value.
@@ -253,13 +292,7 @@ def get_current_asset_value(symbol: str, live: bool = True) -> Dict[str, float]:
         products = symbol.split("/")
         symbol = "".join(products)
 
-    endpoint = None
-
-    if live:
-        endpoint = f"{__live_trade__}/v2/positions"
-    else:
-        endpoint = f"{__paper_trade__}/v2/positions"
-
+    endpoint = get_alpaca_endpoint("trade", "/v2/positions", live)
     response = get(endpoint)
     positions = get_json_data(response)
 
@@ -274,25 +307,28 @@ def get_current_asset_value(symbol: str, live: bool = True) -> Dict[str, float]:
     return {}
 
 
-def get_latest_crypto_quote(symbol: str) -> float:
+def get_latest_crypto_quote(symbol: str, live: bool = False) -> float:
     """Get the latest quote for a cryptocurrency.
 
     Retrieves the latest bid price for a specific cryptocurrency symbol from the crypto quote API.
 
     Args:
         symbol (str): The symbol of the cryptocurrency (e.g., "BTC/USD").
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         float: The latest bid price for the cryptocurrency.
     """
-    url = f"{__live_data__}/v1beta3/crypto/us/latest/quotes"
-    response = get(url, params={"symbols": symbol})
+    endpoint = get_alpaca_endpoint(
+        "data", "/v1beta3/crypto/us/latest/quotes", live=live
+    )
+    response = get(endpoint, params={"symbols": symbol})
     data = get_json_data(response)
     # 'bp' is the last bid price
     return float(data["quotes"][symbol]["bp"])
 
 
-def get_latest_stock_quote(symbol: str, feed: str = "sip") -> float:
+def get_latest_stock_quote(symbol: str, feed: str = "sip", live: bool = False) -> float:
     """Get the latest quote for a stock.
 
     Retrieves the latest bid price for a specific stock symbol from the stock quote API.
@@ -300,19 +336,20 @@ def get_latest_stock_quote(symbol: str, feed: str = "sip") -> float:
     Args:
         symbol (str): The symbol of the stock (e.g., "AAPL").
         feed (str, optional): The data feed to use. Default is "sip".
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         float: The latest bid price for the stock.
     """
-    url = f"{__live_data__}/v2/stocks/quotes/latest"
-    response = get(url, params={"symbols": symbol, "feed": feed})
+    endpoint = get_alpaca_endpoint("data", "/v2/stocks/quotes/latest", live)
+    response = get(endpoint, params={"symbols": symbol, "feed": feed})
     data = get_json_data(response)
     # 'bp' is the last bid price
     return float(data["quotes"]["quotes"][symbol]["bp"])
 
 
 def get_current_market_price(
-    symbol: str, feed: str = "sip", asset_class: str = "stock"
+    symbol: str, feed: str = "sip", asset_class: str = "stock", live: bool = False
 ) -> float:
     """Get the current market price of an asset.
 
@@ -322,6 +359,7 @@ def get_current_market_price(
         symbol (str): The symbol of the asset (e.g., "AAPL", "BTC/USD").
         feed (str, optional): The data feed to use. Default is "sip".
         asset_class (str, optional): The class of the asset ("stock" or "crypto"). Default is "stock".
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         float: The current market price of the asset.
@@ -330,33 +368,35 @@ def get_current_market_price(
         ValueError: If an unsupported asset class is provided.
     """
     if asset_class == "crypto":
-        return get_latest_crypto_quote(symbol)
+        return get_latest_crypto_quote(symbol, live)
     elif asset_class == "stock":
-        return get_latest_stock_quote(symbol, feed)
+        return get_latest_stock_quote(symbol, feed, live)
     else:
         raise ValueError(f"Unsupported asset class: {asset_class}")
 
 
-def get_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
+def get_crypto_candlesticks(
+    loc: str, params: Dict[str, Any], live: bool = True
+) -> Dict[str, List[Dict]]:
     """Fetches historical crypto candlestick data from the Alpaca API.
 
     Args:
         loc (str): Crypto location, e.g., "us".
         params (dict[str, Any]): Parameters for the API request.
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         Dict[str, List[Dict]]: Historical candlestick data for specified symbols.
     """
-    url = f"{__live_data__}/v1beta3/crypto/{loc}/bars"
-
-    response = get(url, params=params)
-
+    endpoint = get_alpaca_endpoint("data", f"/v1beta3/crypto/{loc}/bars", live)
+    response = get(endpoint, params=params)
     json_data = get_json_data(response)
-
     return json_data.get("bars", {})
 
 
-def page_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
+def page_crypto_candlesticks(
+    loc: str, params: Dict[str, Any], live: bool = False
+) -> Dict[str, List[Dict]]:
     """Fetches paginated historical crypto candlestick data from the Alpaca API.
 
     Retrieves candlestick data for specified crypto assets, handling pagination if applicable.
@@ -364,14 +404,15 @@ def page_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List
     Args:
         loc (str): Crypto location, e.g., "us".
         params (Dict[str, Any]): Parameters for the API request.
+        live (bool, optional): Whether to use live trading data. Default is False.
 
     Returns:
         Dict[str, List[Dict]]: Historical candlestick data for specified symbols, paginated if needed.
     """
     all_bars = defaultdict(list)
-    url = f"{__live_data__}/v1beta3/crypto/{loc}/bars"
+    endpoint = get_alpaca_endpoint("data", f"/v1beta3/crypto/{loc}/bars", live=live)
 
-    for response in page(url, params):
+    for response in page(endpoint, params):
         json_data = get_json_data(response)
         bars = json_data.get("bars", {})
 
@@ -397,7 +438,12 @@ def create_order(
     Places a new order for the given account. An order request may be rejected if the account is not authorized for trading, or if the tradable balance is insufficient to fill the order.
 
     Args:
-        live (bool): Whether to execute the trade in a live environment. Defaults to False for paper trading.
+        symbol (str): The symbol of the asset to trade (e.g., "AAPL").
+        quantity (float): The quantity of the asset to trade.
+        side (str): The side of the order ("buy" or "sell").
+        type_ (str): The order type ("market", "limit", etc.).
+        time_in_force (str): The time in force for the order ("gtc", "ioc", etc.).
+        live (bool, optional): Whether to execute the trade in a live environment. Defaults to False for paper trading.
 
     Returns:
         Dict: Response from the Alpaca API.
@@ -412,14 +458,38 @@ def create_order(
     }
 
     # Choose the correct endpoint based on the `live` flag
-    endpoint = None
-
-    if live:
-        endpoint = f"{__live_trade__}/v2/orders"
-    else:
-        endpoint = f"{__paper_trade__}/v2/orders"
+    endpoint = get_alpaca_endpoint("trade", "/v2/orders", live=live)
 
     # Execute the POST request
     response = post(endpoint, json=payload)
 
     return get_json_data(response)
+
+
+def get_amount_from_orders(symbol: str, live: bool = False) -> float:
+    """Get the total quantity of a specified asset from closed and filled orders.
+
+    Retrieves the total filled quantity of a specified asset from closed orders.
+
+    Args:
+        symbol (str): The symbol of the asset (e.g., "AAPL", "BTC/USD").
+        live (bool, optional): Whether to use live trading data. Default is False.
+
+    Returns:
+        float: The total filled quantity of the asset from closed orders.
+    """
+    total_qty = 0.0
+    endpoint = get_alpaca_endpoint("broker", "/v2/orders", live=live)
+    params = {
+        "status": "closed",
+        "symbol": symbol,
+    }
+
+    response = get(endpoint, params=params)
+    orders = get_json_data(response)
+
+    for order in orders:
+        if order["symbol"] == symbol and order["status"] == "filled":
+            total_qty += float(order["filled_qty"])
+
+    return total_qty
