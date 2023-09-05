551e7cc4a8abbe074cf42058a1743e9c785cba5b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-04 22:57:37 -0400

    refactor: Enhance Alpaca API module with URL organization and error handling
    - Reorganized Alpaca API URLs into distinct categories (Broker, Trade, Market Data).
    - Introduced generator function `page` for paginating API responses.
    - Refactored error handling to handle JSON decoding errors consistently.
    - Improved code readability and maintainability by eliminating repetitive code.
    
    This commit significantly improves the Alpaca API module in `coinbot/api/alpaca.py` by reorganizing URLs, enhancing error handling, and refactoring code for better clarity. The introduction of the `page` generator function and consistent JSON decoding error handling contribute to the overall robustness of the API interactions in the CoinBot project.

--- coinbot/api/alpaca.py
+++ coinbot/api/alpaca.py
@@ -5,7 +5,7 @@ import time
 from collections import defaultdict
 from json import JSONDecodeError
 from os import getenv
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, Generator, List, Optional, Union
 
 import requests
 from dotenv import load_dotenv
@@ -26,11 +26,21 @@ __limit__: float = 1 / (200 / 60)
 # Timeout value for HTTP requests.
 __timeout__: int = 30
 
-# Paper trading URL differs from live trading
-__paper__ = "https://paper-api.alpaca.markets"
+#
+# NOTE: Alpaca has multiple sub-domains depending on the given context.
+#
 
-# Use the live trading URL
-__alpaca__ = "https://api.alpaca.markets"
+# BROKER API
+__paper_broker__ = "https://broker-api.sandbox.alpaca.markets"
+__live_broker__ = "https://broker-api.alpaca.markets"
+
+# TRADING API
+__paper_trade__ = "https://paper-api.alpaca.markets"
+__live_trade__ = "https://api.alpaca.markets"
+
+# Market Data API
+__paper_data__ = "https://data.sandbox.alpaca.markets"
+__live_data__ = "https://data.alpaca.markets"
 
 
 class Auth(AuthBase):
@@ -103,17 +113,7 @@ def get(url: str, params: Optional[Dict] = None) -> Response:
             timeout=__timeout__,
         )
 
-        if response.status_code != 200:
-            error_message = response.json()["message"]
-
-        if response.status_code == 400:
-            raise RequestException(f"400 Bad Request: {error_message}")
-        elif response.status_code == 401:
-            raise RequestException(f"401 Unauthorized: {error_message}.")
-        elif response.status_code == 403:
-            raise RequestException(f"403 Forbidden: {error_message}.")
-        else:
-            return response
+        return response
     except RequestException as error:
         raise RequestException(error)
 
@@ -125,9 +125,9 @@ def post(url: str, json: Optional[Dict] = None) -> Response:
         path: The API endpoint to be requested.
         json: (optional) JSON payload to be sent with the request.
 
-    Returns: The response of the POST request.
+    Returns:
+        The response of the POST request.
     """
-
     time.sleep(__limit__)
 
     try:
@@ -138,24 +138,72 @@ def post(url: str, json: Optional[Dict] = None) -> Response:
             timeout=__timeout__,
         )
 
-        if response.status_code != 200:
-            error_message = response.json()["message"]
-
-        if response.status_code == 400:
-            raise RequestException(f"400 Bad Request: {error_message}")
-        elif response.status_code == 401:
-            raise RequestException(f"401 Unauthorized: {error_message}.")
-        elif response.status_code == 403:
-            raise RequestException(f"403 Forbidden: {error_message}.")
-        else:
-            return response
+        return response
     except RequestException as error:
         raise RequestException(error)
 
 
-def get_clock() -> Dict[str, Union[str, bool]]:
+def page(url: str, params: Optional[Dict] = None) -> Generator[Response, None, None]:
+    """Pages through API responses.
+
+    Args:
+        url (str): API endpoint URL.
+        params (Optional[Dict]): Query parameters for the API call.
+
+    Yields:
+        Response: HTTP Response object for each page.
     """
-    Returns the market clock.
+
+    next_page_token = None  # Initialize the page token
+
+    while True:
+        if next_page_token is not None:
+            params = params or {}
+            params["page_token"] = next_page_token
+
+        # Perform a GET request to the specified API path.
+        response = get(url, params=params)
+        yield response
+
+        try:
+            json_data = response.json()
+            next_page_token = json_data.get("next_page_token")
+        except JSONDecodeError as message:
+            logging.error(message)
+            raise RequestException(f"Error {response.status_code}: {response.text}")
+
+        if next_page_token is None:
+            break
+
+
+def get_json_data(response: Response) -> Dict[str, Any]:
+    """Process the HTTP Response.
+
+    Args:
+        response (Response): The HTTP Response object.
+
+    Returns:
+        Dict: The JSON-decoded content of the HTTP response.
+
+    Raises:
+        RequestException: If the response contains an error.
+    """
+    try:
+        json_data = response.json()
+
+        if response.status_code != 200:
+            error_message = json_data.get("message", "Unknown error")
+            raise RequestException(f"Error ({response.status_code}): {error_message}")
+
+        return json_data
+
+    except JSONDecodeError as message:
+        logging.error(message)
+        raise RequestException(f"Error ({response.status_code}): {response.text}")
+
+
+def get_clock() -> Dict[str, Union[str, bool]]:
+    """Returns the market clock.
 
     The clock API serves the current market timestamp, whether or not the market is currently open, as well as the times of the next market open and close.
 
@@ -164,23 +212,13 @@ def get_clock() -> Dict[str, Union[str, bool]]:
     Returns:
         Dict: A dictionary representing whether the market is open or closed.
     """
-    # NOTE: get uses time.sleep(__limit__) which is set for Alpacas API
-    response = get(f"{__alpaca__}/v2/clock")
+    response = get(f"{__live_trade__}/v2/clock")
 
-    if response.status_code != 200:
-        error_message = response.json()["message"]
-        raise RequestException(f"Unexpected Error: {error_message}")
-
-    try:
-        return response.json()
-    except JSONDecodeError as message:
-        logging.error(message)
-        raise RequestException(f"Error {response.status_code}: {response.text}")
+    return get_json_data(response)
 
 
 def get_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
-    """
-    Fetches historical crypto candlestick data from the Alpaca API.
+    """Fetches historical crypto candlestick data from the Alpaca API.
 
     Args:
         loc (str): Crypto location, e.g., "us".
@@ -189,22 +227,17 @@ def get_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[
     Returns:
         Dict[str, List[Dict]]: Historical candlestick data for specified symbols.
     """
-    url = f"https://data.alpaca.markets/v1beta3/crypto/{loc}/bars"
-    # NOTE: `get` is a magic function that handles `requests` under the hood.
-    response: Response = get(url, params=params)
-    json_data: Dict[str, Any] = response.json()
+    url = f"{__live_data__}/v1beta3/crypto/{loc}/bars"
 
-    if response.status_code != 200:
-        error = json_data.get("message", "Error getting candlesticks from Alpaca API")
-        logging.error(f"GetError: {response.status_code}: {error}")
-        return {}
+    response = get(url, params=params)
+
+    json_data = get_json_data(response)
 
     return json_data.get("bars", {})
 
 
 def page_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List[Dict]]:
-    """
-    Fetches paginated historical crypto candlestick data from the Alpaca API.
+    """Fetches paginated historical crypto candlestick data from the Alpaca API.
 
     Retrieves candlestick data for specified crypto assets, handling pagination if applicable.
 
@@ -215,35 +248,20 @@ def page_crypto_candlesticks(loc: str, params: Dict[str, Any]) -> Dict[str, List
     Returns:
         Dict[str, List[Dict]]: Historical candlestick data for specified symbols, paginated if needed.
     """
-    url = f"https://data.alpaca.markets/v1beta3/crypto/{loc}/bars"
     all_bars = defaultdict(list)
+    url = f"{__live_data__}/v1beta3/crypto/{loc}/bars"
 
-    while True:
-        # NOTE: `get` is a magic function that handles `requests` under the hood.
-        response: Response = get(url, params=params)
-        json_data: Dict[str, Any] = response.json()
-
-        if response.status_code != 200:
-            error = json_data.get(
-                "message", "Error paging candlesticks from Alpaca API"
-            )
-            logging.error(f"PageError: {response.status_code}: {error}")
-            break
-
+    for response in page(url, params):
+        json_data = get_json_data(response)
         bars = json_data.get("bars", {})
+
         if not bars:
             break
 
         for asset_pair, asset_bars in bars.items():
             all_bars[asset_pair].extend(asset_bars)
 
-        next_page_token = json_data.get("next_page_token", None)
-        if next_page_token is None:
-            break
-
-        params["page_token"] = next_page_token
-
-    return dict(all_bars)  # Convert defaultdict back to a regular dictionary.
+    return dict(all_bars)
 
 
 def create_order(
@@ -254,8 +272,7 @@ def create_order(
     time_in_force: str,
     live: bool = False,
 ) -> Dict:
-    """
-    Create an Order
+    """Create an Order.
 
     Places a new order for the given account. An order request may be rejected if the account is not authorized for trading, or if the tradable balance is insufficient to fill the order.
 
@@ -275,22 +292,9 @@ def create_order(
     }
 
     # Choose the correct endpoint based on the `live` flag
-    endpoint = f"{__alpaca__}/v2/orders" if live else f"{__paper__}/v2/orders"
+    endpoint = f"{__live_trade__}/v2/orders" if live else f"{__paper_trade__}/v2/orders"
 
     # Execute the POST request
     response = post(endpoint, json=payload)
 
-    try:
-        json_data = response.json()
-
-        if response.status_code != 200:
-            error_message = json_data.get("message", "Unknown error")
-            raise RequestException(
-                f"Unexpected Error ({response.status_code}): {error_message}"
-            )
-
-        logging.info(f"Successfully created order: {json_data}")
-        return json_data
-    except JSONDecodeError as message:
-        logging.error(f"JSONDecodeError: {message}")
-        raise RequestException(f"Error {response.status_code}: {response.text}")
+    return get_json_data(response)
