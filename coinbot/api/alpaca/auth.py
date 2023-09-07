3d7c91a2bebc58d7cff77fff3b0d683d02b18062
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-07 12:30:19 -0400

    refactor: Update AlpacaAuth class docstring and constructor
    - Added usage examples to the AlpacaAuth class docstring.
    - Revised the constructor to include optional key and secret parameters for explicit API credential input.
    - Improved error handling and logging for missing or incomplete API credentials.
    
    This commit enhances the documentation and usability of the AlpacaAuth class.

--- coinbot/api/alpaca/auth.py
+++ coinbot/api/alpaca/auth.py
@@ -1,8 +1,21 @@
 """
 coinbot/api/alpaca/auth.py
+
+usage:
+    # Create an instance of AlpacaAuth with API keys loaded from a .env file
+    # NOTE: This is preferred for security purposes.
+    auth = AlpacaAuth(path=".env")
+
+    # Alternatively, provide API keys explicitly
+    # NOTE: This is discouraged for security purposes.
+    auth = AlpacaAuth(key="your_api_key_here", secret="your_api_secret_here")
+
+    # Access the API key and secret for further use
+    api_key = auth.key
+    api_secret = auth.secret
 """
 import os
-from typing import Dict
+from typing import Dict, Optional
 
 import dotenv
 from requests.auth import AuthBase
@@ -12,45 +25,71 @@ from coinbot import __agent__, __source__, __version__, logging
 
 
 class AlpacaAuth(AuthBase):
-    """
-    Create and return an HTTP request with authentication headers.
+    """Create and return an HTTP request with authentication headers."""
 
-    Args:
-        path (str): The path to the .env file containing API credentials.
-            If not provided, the default is ".env".
-    """
-
-    def __init__(self, path: str = ".env"):
+    def __init__(
+        self,
+        key: Optional[str] = None,
+        secret: Optional[str] = None,
+        path: Optional[str] = None,
+    ):
         """
-        Create an instance of the AlpacaAuth class.
+        Initialize an instance of the AlpacaAuth class with API credentials.
 
         Args:
-            path (str): The path to the .env file containing API credentials.
-                If not provided, the default is ".env".
+            key (str, optional): The Alpaca API key. If not provided, it will be
+                retrieved from environment variables.
+            secret (str, optional): The Alpaca API secret key. If not provided, it
+                will be retrieved from environment variables.
+            path (str, optional): The path to the .env file containing API credentials.
         """
-        # Try to load the .env file
-        env_loaded = dotenv.load_dotenv(path)
+        # Try to load environment variables from the .env file
+        if not key and not secret:
+            env_loaded = dotenv.load_dotenv(path)
+        else:
+            env_loaded = False
 
+        # Assign API keys and secrets
+        # Priority: Environment variables (Paper over Live) > Constructor arguments > Empty string
+        if env_loaded:
+            self.key = (
+                os.getenv("PAPER_ALPACA_API_KEY") or os.getenv("ALPACA_API_KEY") or ""
+            )
+            self.secret = (
+                os.getenv("PAPER_ALPACA_API_SECRET")
+                or os.getenv("ALPACA_API_SECRET")
+                or ""
+            )
+        elif key is not None and secret is not None:
+            self.key = key
+            self.secret = secret
+        else:
+            # Fallback to empty strings if no other options are available
+            self.key = ""
+            self.secret = ""
+
+        # Log if environment variables couldn't be loaded
         if not env_loaded:
-            # Log a warning but continue execution
-            print(f"Warning: Failed to load {path}")
-
-        # Determine which keys to use, preferring Paper over Live
-        self.key = (
-            os.getenv("PAPER_ALPACA_API_KEY") or os.getenv("ALPACA_API_KEY") or ""
-        )
-        self.secret = (
-            os.getenv("PAPER_ALPACA_API_SECRET") or os.getenv("ALPACA_API_SECRET") or ""
-        )
-
-        if not self.key or not self.secret:
             logging.warning(
-                "An issue with Key and/or Secret was detected. "
-                "Some functionalities may be restricted."
+                "Could not load environment variables. Falling back to provided keys or public endpoints."
             )
 
-        logging.debug(f"Alpaca API Key: ****{self.key[-4:]}")
-        logging.debug(f"Alpaca API Secret: ****{self.secret[-4:]}")
+        # Log the status of keys and secrets
+        if self.key and self.secret:
+            # Mask the last 4 characters of both the key and the secret for security
+            logging.debug(f"Alpaca API Key: ****{self.key[-4:]}")
+            logging.debug(f"Alpaca API Secret: ****{self.secret[-4:]}")
+        elif not self.key or not self.secret:
+            # Log an error if either the key or secret is missing, suggesting a likely user error
+            logging.error(
+                "Either Key or Secret is missing. "
+                "Revise your source code and environment variables to ensure both are passed as arguments."
+            )
+        else:
+            # Log a warning if both the key and secret are missing
+            logging.warning(
+                "Key and Secret are missing. Fallback to public endpoints with limited access."
+            )
 
     def __call__(self, request: PreparedRequest) -> PreparedRequest:
         """
