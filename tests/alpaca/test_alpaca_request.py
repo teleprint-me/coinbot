6d4f121ccdb53e9f52d373f77a1285eb431d3a46
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-09-07 13:50:38 -0400

    test: Enhance AlpacaRequest test coverage and pagination testing
    - Updated the test cases in `test_alpaca_request.py` to use `_request` instead of directly mocking `requests.get` and `requests.post` for better encapsulation.
    - Added a new test case `test_page_request` to test pagination handling in the `page` method.
    - Created a `MockResponse` class to simulate API responses for testing.
    - These updates improve the test coverage for `AlpacaRequest` and pagination functionality.

--- tests/alpaca/test_alpaca_request.py
+++ tests/alpaca/test_alpaca_request.py
@@ -18,21 +18,66 @@ def test_init():
 
 
 def test_get_request(mocker):
-    mocker.patch("requests.get", return_value="some_response")
+    mocker.patch(
+        "coinbot.api.alpaca.AlpacaRequest._request",
+        return_value="get_response",
+    )
     req = AlpacaRequest()
     response = req.get("https://some.url")
-    assert response == "some_response"
+    assert response == "get_response"
 
 
 def test_post_request(mocker):
-    mocker.patch("requests.post", return_value="post_response")
+    mocker.patch(
+        "coinbot.api.alpaca.AlpacaRequest._request",
+        return_value="post_response",
+    )
     req = AlpacaRequest()
     response = req.post("https://some.url", json={"key": "value"})
     assert response == "post_response"
 
 
+class MockResponse:
+    def __init__(self, json_data, status_code, headers=None):
+        self.json_data = json_data
+        self.status_code = status_code
+        self.headers = headers
+
+    def json(self):
+        return self.json_data
+
+
+def test_page_request(mocker):
+    mock_values = [
+        MockResponse({"data": "page1", "next_page_token": "token1"}, 200),
+        MockResponse({"data": "page2", "next_page_token": "token2"}, 200),
+        MockResponse({"data": "page3", "next_page_token": None}, 200),
+    ]
+
+    def side_effect(*args, **kwargs):
+        for value in mock_values:
+            yield value
+
+    mocker.patch(
+        "coinbot.api.alpaca.AlpacaRequest.get",
+        side_effect=side_effect(),
+    )
+    req = AlpacaRequest()
+
+    # Collect responses from the generator
+    responses = [
+        res.json_data["data"]
+        for res in req.page("https://some.url", params={"key": "value"})
+    ]
+
+    assert responses == ["page1", "page2", "page3"]
+
+
 def test_request_exception(mocker):
-    mocker.patch("requests.get", side_effect=RequestException)
+    mocker.patch(
+        "coinbot.api.alpaca.AlpacaRequest._request",
+        side_effect=RequestException,
+    )
     req = AlpacaRequest()
     with pytest.raises(RequestException):
         req.get("https://some.url")
