8955c3525826bf91228daaaaf4d2253fa4033a5f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-25 18:13:43 -0400

    coinbot(feat): Add proof-of-concept for pinging coinbase developer platform

--- coinbot/cb.py
+++ coinbot/cb.py
@@ -1,102 +1,63 @@
 """
 coinbot/cb.py
 """
-import hashlib
-import hmac
-import http.client
+
 import logging
 import os
-import time
 from datetime import datetime
-from typing import Optional, Tuple
 
 import dotenv
 import requests
-from requests.auth import AuthBase
+from cdp.auth.utils.jwt import JwtOptions, generate_jwt
 
-# Initialize Logging
-logging.basicConfig(level=logging.DEBUG)
+if __name__ == "__main__":
+    logger = logging.getLogger("coinbase")
+    logger.setLevel(logging.DEBUG)
 
+    dotenv.load_dotenv(".env")
 
-def get_api_key(path: Optional[str] = ".env") -> Tuple[str, str]:
-    # Try to load the .env file
-    dotenv.load_dotenv(path)
-
-    # Determine which keys to use, preferring Private over Public
+    # Load secrets
     api_key = os.getenv("COINBASE_API_KEY")
     api_secret = os.getenv("COINBASE_API_SECRET")
 
-    logging.debug(f"Loaded API Key: {bool(api_key)}")
-    logging.debug(f"Loaded API Secret: {bool(api_secret)}")
+    if not api_key or not api_secret:
+        raise ValueError("Missing api key and or secret")
 
-    return api_key, api_secret
+    logger.debug(f"Loaded API Key: {bool(api_key)}")
+    logger.debug(f"Loaded API Secret: {bool(api_secret)}")
 
-
-# Common Variables
-api_key, api_secret = get_api_key()
-timestamp = str(int(time.time()))
-method = "GET"
-path = "/v3/brokerage/products/BTC-USD/candles"
-start = int((datetime(2020, 1, 1, 0, 0, 0) - datetime(1970, 1, 1)).total_seconds())
-end = int((datetime(2020, 1, 1, 23, 59, 59) - datetime(1970, 1, 1)).total_seconds())
-params = f"start={start}&end={end}&granularity=ONE_HOUR"
-message = timestamp + method + path + ""
-logging.info(f"Timestamp: {timestamp}")
-logging.info(f"Message: {message}")
-
-
-# HTTP.client Method
-def http_client_method():
-    signature = hmac.new(
-        api_secret.encode("utf-8"),
-        message.encode("utf-8"),
-        digestmod=hashlib.sha256,
-    ).hexdigest()
-    conn = http.client.HTTPSConnection("api.coinbase.com")
-    headers = {
-        "CB-ACCESS-KEY": api_key,
-        "CB-ACCESS-SIGN": signature,
-        "CB-ACCESS-TIMESTAMP": timestamp,
-        "CB-VERSION": "2022-10-17",
-    }
-    conn.request(method, path + f"?{params}", headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    logging.info(f"HTTP.client Response: {data}")
-
-
-# Requests Method
-class Auth(AuthBase):
-    def __call__(self, request):
-        signature = hmac.new(
-            api_secret.encode("utf-8"), message.encode("utf-8"), hashlib.sha256
-        ).hexdigest()
-        request.headers.update(
-            {
-                "CB-ACCESS-KEY": api_key,
-                "CB-ACCESS-SIGN": signature,
-                "CB-ACCESS-TIMESTAMP": timestamp,
-                "CB-VERSION": "2022-10-17",
-            }
+    # Construct JWT
+    jwt_token = generate_jwt(
+        JwtOptions(
+            api_key_id=api_key,
+            api_key_secret=api_secret,
+            request_method="GET",
+            request_host="api.coinbase.com",  # no https:// here
+            request_path="/api/v3/brokerage/products/BTC-USD/candles",
+            expires_in=30,
         )
-        return request
-
-
-def requests_method():
-    url = "https://api.coinbase.com" + path
-    auth = Auth()
-    response = requests.get(url, params=params, auth=auth)
-    if response.status_code == 200:
-        print("Requests Response:", response.json())
-    else:
-        logging.error(f"Failed to get data: {response.content}")
-
-
-# Main Execution
-if __name__ == "__main__":
-    logging.debug(
-        f"API Key Loaded: {api_key is not None}, API Secret Loaded: {api_secret is not None}"
     )
 
-    http_client_method()
-    requests_method()
+    # Build query
+    params = {
+        "start": int(datetime(2020, 1, 1, 0, 0).timestamp()),
+        "end": int(datetime(2020, 1, 1, 23, 59).timestamp()),
+        "granularity": "ONE_HOUR",
+    }
+
+    # Request data
+    response = requests.get(
+        "https://api.coinbase.com/api/v3/brokerage/products/BTC-USD/candles",
+        headers={
+            "Authorization": f"Bearer {jwt_token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        params=params,
+    )
+
+    # Output
+    if response.status_code == 200:
+        logger.info(response.json())
+    else:
+        logger.error(f"[ERROR] {response.status_code}: {response.content}")
